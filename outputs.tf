1ebd760deaf6b9ed9951216ff32e0d6e9b4b745f
Dan Enman <enmand@gmail.com>
2018-07-09 22:26:12 -0300

    Add vpn_instanve_id and vpn_network_id outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "vpn_public_ip_address" {
 output "vpn_management_ui" {
   value = "https://${aws_eip.pritunl.public_ip}"
 }
+
+output "vpn_instance_id" {
+  value = "${aws_instance.pritunl.id}"
+}
+
+output "vpn_network_id" {
+  value = "${aws_instance.pritunl.primary_network_interface_id}"
+}
